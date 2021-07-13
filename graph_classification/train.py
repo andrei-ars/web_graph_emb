a901027823ec01b52f235ca2c30d5d53f1b084ec
Andrei <z@sp.sp>
2021-07-14 01:25:08 +0300

    Added print

--- graph_classification/train.py
+++ graph_classification/train.py
@@ -64,7 +64,10 @@ def test(loader):
 
      correct = 0
      for data in loader:  # Iterate in batches over the training/test dataset.
-         out = model(data.x, data.edge_index, data.batch)  
+         print("data.x:", data.x)
+         print("data.edge_index:", data.edge_index)
+         print("data.batch:", data.batch)
+         out = model(data.x, data.edge_index, data.batch)
          pred = out.argmax(dim=1)  # Use the class with highest probability.
          correct += int((pred == data.y).sum())  # Check against ground-truth labels.
      return correct / len(loader.dataset)  # Derive ratio of correct predictions.
@@ -75,3 +78,4 @@ for epoch in range(1, 201):
     train_acc = test(train_loader)
     test_acc = test(test_loader)
     print(f'Epoch: {epoch:03d}, Train Acc: {train_acc:.4f}, Test Acc: {test_acc:.4f}')
+

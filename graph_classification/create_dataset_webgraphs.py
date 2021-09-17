105aaac284db3077549244d1272f24086184aa4f
Andrei <z@sp.sp>
2021-09-17 15:36:50 +0300

    update

---
+++ graph_classification/create_dataset_webgraphs.py
@@ -0,0 +1,135 @@
+#from selenium import webdriver
+#from selenium.webdriver.common.by import By
+
+from webgraph_factory.web_graph import WebGraphFactory
+from webgraph_factory.feature_extraction.extractor import FeatureExtractor
+import pickle
+
+
+def create_webgraphs(html_files):
+
+    state_factory = WebGraphFactory(sequence_handler=None)
+    FeatureExtractor.set_config(config=None)
+
+    url = "https://demo1.testgold.dev/login"
+
+    webgraphs = []
+
+    for path in html_files:
+        with open(path) as fp:
+            html = fp.read()
+        webgraph = state_factory.get_webgraph(html, url)
+        webgraphs.append(webgraph)
+
+    return webgraphs
+
+
+def create_dataset(webgraphs, labels):
+    """
+    webgraphs: the list of webgraphs
+    lables: the list of labels
+    """
+    edges = []
+    properties = []
+    features = []
+    assert len(webgraphs) == len(labels)
+
+    for graph_id, webgraph in enumerate(webgraphs):
+        webgraph.visualize()
+        x, edge_index = webgraph.get_data()
+        num_nodes = len(x)
+        label = labels[graph_id]
+        properties.append((graph_id, label, num_nodes))
+        for edge in edge_index:
+            edges.append((graph_id, edge[0], edge[1]))
+        for node_id, feature in enumerate(x):
+            features.append((graph_id, node_id, feature))
+
+    return {'edges': edges, 'properties': properties, 'features': features}
+
+
+def create_dataset_as_list(webgraphs, labels):
+    """
+    webgraphs: the list of webgraphs
+    lables: the list of labels
+    """
+    edges = []
+    properties = []
+    features = []
+    assert len(webgraphs) == len(labels)
+
+    dataset = []
+
+    for graph_id, webgraph in enumerate(webgraphs):
+        #webgraph.visualize()
+        x, edge_index = webgraph.get_data()
+        #num_nodes = len(x)
+        #label = labels[graph_id]
+        #properties.append((graph_id, label, num_nodes))
+        #for edge in edge_index:
+        #    edges.append((graph_id, edge[0], edge[1]))
+        #for node_id, feature in enumerate(x):
+        #    features.append((graph_id, node_id, feature))
+
+        gr = {
+            'id': graph_id, 
+            'label': labels[graph_id],
+            'num_nodes': len(x),
+            'edges': edge_index,
+            'x': x
+        }
+        dataset.append(gr)
+
+    return dataset
+
+
+if __name__ == "__main__":
+
+    url = "https://demo1.testgold.dev/login"
+
+    #options = webdriver.ChromeOptions()
+    #options.add_argument('--ignore-certificate-errors')
+    #options.add_argument("--test-type")
+    #options.add_argument("--silent")
+    #driver = webdriver.Chrome(chrome_options=options, executable_path='../chromedriver')
+    #driver.get(url)
+    #html = driver.page_source
+    
+    with open('init.html') as fp:
+        html = fp.read()
+
+    print("url:", url)
+    print("html size:", len(html))
+
+    state_factory = WebGraphFactory(sequence_handler=None)
+    #html = self.get_html_code()
+    webgraph = state_factory.get_webgraph(html, url)
+    FeatureExtractor.set_config(config=None)
+    print("webgraph:", webgraph)
+    print("output:", webgraph.torch)
+    print("type:", type(webgraph.torch))
+    #webgraph.visualize()
+    #x, edge_index = webgraph.get_data()
+    #print(x)
+    #print(edge_index)
+    #webgraph.visualize()
+
+    #print("Saving webgraphes into files...")
+    #with open("./dataset/graph_edges.csv", "wt") as fp_edges:
+    #    with open("./dataset/graph_properties.csv", "wt") as fp_properties:
+    #        with open("./dataset/node_features.csv", "wt") as fp_features:
+    #            graph_id = 0
+    #            webgraph.save_to_csv_files(graph_id, fp_edges, fp_properties, fp_features)
+
+
+    labels = ['login', 'other', 'other']
+    html_files = ['html/0.html', 'html/1.html', 'html/2.html']
+
+    webgraphs = create_webgraphs(html_files)
+    #dataset = create_dataset(webgraphs, labels)
+    dataset = create_dataset_as_list(webgraphs, labels)
+    
+    print(dataset)
+
+    with open("dataset.dump", "wb") as fp: 
+        pickle.dump(dataset, fp)
---
+++ graph_classification_test/dataload_data.py
@@ -0,0 +1,167 @@
+"""
+v2 - using torch_geometric.data import Data
+
+See
+https://docs.dgl.ai/en/0.6.x/new-tutorial/6_load_data.html
+https://docs.dgl.ai/en/0.6.x/guide/data-dataset.html#guide-data-pipeline-dataset
+https://pytorch-geometric.readthedocs.io/en/latest/_modules/torch_geometric/datasets/tu_dataset.html
+
+"""
+import numpy as np
+#import dgl
+#from dgl.data import DGLDataset
+import torch
+import os
+import pickle
+
+from torch_geometric.data import Data
+from torch_geometric.data import Dataset
+
+"""
+The format of data files:
+
+    graph_edges.csv: containing three columns:
+        graph_id: the ID of the graph.
+        src: the source node of an edge of the given graph.
+        dst: the destination node of an edge of the given graph.
+
+    graph_properties.csv: containing three columns:
+        graph_id: the ID of the graph.
+        label: the label of the graph.
+        num_nodes: the number of nodes in the graph.
+
+    node_features:
+        graph_id: int
+        node_id: int
+        features: 1d-vector
+
+"""
+
+#edges = pd.read_csv('./graph_edges.csv')
+#properties = pd.read_csv('./graph_properties.csv')
+#edges.head()
+#properties.head()
+
+
+class WebGraphDataset():
+    """ A dataset for webgraph classification problem.
+    Dataset should be a list of graphs and labels.
+    getitem should return a pair (graph, label)
+    """
+    #def __init__(self, path=None):
+    #    super().__init__()
+
+    def __init__(self):
+
+        #with open(path, "rb") as fp:
+        #    dataset = pickle.load(fp)
+        #    print(dataset)
+        self.process()
+       
+    @property
+    def raw_file_names(self):
+        return ['some_file_1', 'some_file_2', ...]
+
+    @property
+    def processed_file_names(self):
+        return ['data_1.pt', 'data_2.pt', ...]
+
+    def download(self):
+        # Download to `self.raw_dir`.
+        path = download_url(url, self.raw_dir)
+        ...
+
+    def process(self):
+        #edges = pd.read_csv('./graph_edges.csv')
+        #properties = pd.read_csv('./graph_properties.csv')
+        self.graphs = []
+        self.labels = []
+
+        fp = open("data/dataset.dump", "rb")
+        dataset = pickle.load(fp)
+        fp.close()
+        label2index = {'login': 0, 'other': 1}
+
+        for graph_data in dataset:
+            graph_id = graph_data['id']
+            label = graph_data['label']
+            label_index = label2index[label]
+            num_nodes = graph_data['num_nodes']
+            edges = graph_data['edges']
+            x = graph_data['x']
+            print(graph_id, label, label2index[label])
+
+            src = np.array([edge[0] for edge in edges])
+            dst = np.array([edge[1] for edge in edges])
+            print("src:", src)
+            print("dst:", dst)
+            # Create a graph and add it to the list of graphs and labels.
+            #g = dgl.graph((src, dst), num_nodes=num_nodes)
+            
+            edge_index = torch.tensor([src, dst])
+            x = torch.tensor(np.array(x))
+            y = torch.tensor([label_index])
+
+            #g = Data(x=x, edge_index=edge_index.t().contiguous())
+            g = Data(x=x, y=y, edge_index=edge_index)
+
+            print(g)
+            #print(g.ndata['x'].shape)
+            #g.ndata['x'] = torch.tensor(np.array(x))
+            #g.x = torch.tensor(np.array(x))
+            #print("g.x:", g.x.shape)
+            #g.y = torch.tensor(label_index)
+            #g.edge_index = torch.tensor([src, dst])
+
+            self.graphs.append(g)
+            self.labels.append(label_index)
+        
+        print(self.labels)
+        self.labels = torch.LongTensor(self.labels)
+
+        self.num_node_features = 106
+        self.num_classes = 2
+
+        """
+        # Create a graph for each graph ID from the edges table.
+        # First process the properties table into two dictionaries with graph IDs as keys.
+        # The label and number of nodes are values.
+        label_dict = {}
+        num_nodes_dict = {}
+        for _, row in properties.iterrows():
+            label_dict[row['graph_id']] = row['label']
+            num_nodes_dict[row['graph_id']] = row['num_nodes']
+            
+        # For the edges, first group the table by graph IDs.
+        edges_group = edges.groupby('graph_id')
+        
+        # For each graph ID...
+        for graph_id in edges_group.groups:
+            # Find the edges as well as the number of nodes and its label.
+            edges_of_id = edges_group.get_group(graph_id)
+            src = edges_of_id['src'].to_numpy()
+            dst = edges_of_id['dst'].to_numpy()
+            num_nodes = num_nodes_dict[graph_id]
+            label = label_dict[graph_id]
+            
+            # Create a graph and add it to the list of graphs and labels.
+            g = dgl.graph((src, dst), num_nodes=num_nodes)
+            self.graphs.append(g)
+            self.labels.append(label)
+            
+        # Convert the label list to tensor for saving.
+        self.labels = torch.LongTensor(self.labels)
+        """
+        
+    def __getitem__(self, idx):
+        return self.graphs[idx] #, self.labels[i]
+    
+    def __len__(self):
+        return len(self.graphs)
+
+
+if __name__ == "__main__":
+
+    dataset = WebGraphDataset("data/dataset.dump")
+    #graph, label = dataset[0]
+    #print(graph, label)
